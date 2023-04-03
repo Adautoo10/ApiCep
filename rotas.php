62b50513ea89cf18ea4418449b73525befc40ed9
Adautoo10 <adauto.facin05@gmail.com>
2023-04-03 08:12:02 -0300

    Atualizando Rotas

--- rotas.php
+++ rotas.php
@@ -8,30 +8,36 @@ switch($url)
 {
 //Exemplo de acesso
 
+
+    //http://localhost:8000/endereco/by-cep?cep=17210580
     case '/endereco/by-cep':
-    EnderecoController::getLogradouroByCep();
-    break;
-
-    case '/logradouro/by-bairro':
-    EnderecoController::getLogradouroByBairroAndCidade();
-    break;
-
-    case '/cep/by-logradouro':
-        EnderecoController::getCepByLogradouro();
-    break;
-
-    case '/cidade/by-uf':
-    EnderecoController::getCidadesByUf();
-    break;
-
-    case 'bairro/by-cidade':
-        EnderecoController::getBairrosByIdCidade();
-    break;
-
-    default:
-        http_response_code(403);
-    break;
-
-}
-
-   
+        EnderecoController::getLogradouroByCep();
+        break;
+    
+        //http://localhost:8000/logradouro/by-bairro?id_cidade=4874&bairro=Jardim América
+        case '/logradouro/by-bairro':
+        EnderecoController::getLogradouroByBairroAndCidade();
+        break;
+    
+        //http://localhost:8000/cep/by-logradouro?logradouro=Rua Raphael de Almeida Lei
+        case '/cep/by-logradouro':
+            EnderecoController::getCepByLogradouro();
+        break;
+    
+        //http://localhost:8000/cidade/by-uf?uf=SP
+        case '/cidade/by-uf':
+        EnderecoController::getCidadesByUf();
+        break;
+    
+        //http://localhost:8000/bairro/by-cidade?id=4874
+        case 'bairro/by-cidade':
+            EnderecoController::getBairrosByIdCidade();
+        break;
+    
+        default:
+            http_response_code(403);
+        break;
+    
+    }
+    
+       
